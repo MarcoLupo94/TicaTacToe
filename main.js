1c67208dc6c580b832c40e85cdb29465c9344c0b
MarcoLupo94 <lupo.marco94@gmail.com>
2022-07-17 17:34:57 +0100

    updated scores to x and o but need to fix bug on restart

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 
- //game module
- const Game = (() =>{
-
- })()
   //Gameboard Module
 const GameBoard = (function(){
   let x = "x";
   let o = "o";
   let board =  new Array(9);
-  const render = () => {
+  let player = x;
+  render();
+
+
+  $('.box').on('click', (event) => {
+    player === x ? player = o : player = x;
+    const position = $(event.currentTarget).attr('id');
+    changeBoard(position);
+    score();
+  });
+
+  function render(){
     for (let i =0; i < 9; i++) {
       board[i] = ("");
       let div = $('<div>');
@@ -18,44 +25,42 @@ const GameBoard = (function(){
   };
 };
 
-  const score = () => {
+  function score() {
     for (let i =0; i < 9; i++)  {
       $(`#${i}`).text(board[i]);
     };
 
   };
-  const changeBoard = (position) => {
-    board[position] = "X";
-
-    // if (player.type = "x"){
-    //   board[position].type = "X";
-    // }
-    // else {
-    //   board[position].type = "X";
-    // }
-
+  function changeBoard(position){
+    if (player === x && board[position] === "" ){
+      board[position] = "X";
+    }
+    else if (player === o && board[position] === ""){
+      board[position]= "O";
+    }
   };
 
-return { render , score, changeBoard }
+  function restart(){
+    board = new Array(9);
+    $('.box').remove();
+    render();
+  };
+
+
+return {  restart }
 
 })();
 
-//Player factory function
-const Player = (name,type) => {
-  return{name,type};
-};
-
-GameBoard.render();
-GameBoard.score();
-$('.box').on('click', (event) => {
-  const position = $(event.currentTarget).attr('id');
-  console.log(position);
-
-  GameBoard.changeBoard(position);
-  GameBoard.score();
+ //game module
+ const Game = (() =>{
+  $('#restart').on('click',restart);
 
 
-});
+  function restart() {
+    GameBoard.restart();
+  }
+  return {} ;
 
+ })();
 
 });
